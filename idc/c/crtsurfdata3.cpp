43d8e0878805b790fd8af4b4efec435147de7e44
Celees <wydxry@163.com>
2022-05-25 19:43:48 +0800

    add crtsurfdata3.cpp crtsurfdata4.cpp and update makefile

--- idc/c/crtsurfdata3.cpp
+++ idc/c/crtsurfdata3.cpp
@@ -1,8 +1,8 @@
 /*
- *  程序名：crtsurfdata2.cpp  本程序用于生成全国气象站点观测的分钟数据
- *  作者：wydxry
+ *  程序名：crtsurfdata3.cpp  本程序用于生成全国气象站点观测的分钟数据
+ *  作者：Celees
  *  时间：2022.05.25 
- *  任务：全国气象站点参数加载到容器中
+ *  任务：模拟生成全国气象站点分钟观测数据
 */
 
 #include "_public.h"
@@ -23,10 +23,31 @@ struct st_site
     double height; // 海拔高度
 };
 
+// 全国气象站点分钟观测数据结构
+struct st_surfdata
+{
+    char siteid[11];     // 站号
+    char datatime[21];   // 数据时间：格式yyyymmddhh24miss
+    int  t;              // 气温：单位，0.1摄氏度。
+    int  p;              // 气压：0.1百帕。
+    int  u;              // 相对湿度，0-100之间的值。
+    int  wd;             // 风向，0-360之间的值。
+    int  wf;             // 风速：单位0.1m/s
+    int  r;              // 降雨量：0.1mm。
+    int  vis;            // 能见度：0.1米。
+};
+
 // 存放全国气象站点参数的容器
 vector<struct st_site> vsites;
 
+// 存放全国气象站点分钟观测数据的容器
+vector<struct st_surfdata> vsurfdata;  
+
+// 把站点参数文件中加载到vstcode容器中
 bool LoadSite(const char *inifile);
+
+// 模拟生成全国气象站点分钟观测数据，存放在vsurfdata容器中
+void CrtSurfData();
 
 int main(int argc, char const *argv[])
 {
@@ -53,7 +74,7 @@ int main(int argc, char const *argv[])
         printf("成功打开日志文件.\n");
     }
 
-    logfile.Write("crtsurfdata2 开始运行。\n");
+    logfile.Write("crtsurfdata3 开始运行。\n");
 
     // 把站点参数文件中加载到vsites容器中
     auto res = LoadSite(argv[1]);
@@ -63,7 +84,10 @@ int main(int argc, char const *argv[])
         printf("未成功载入文件\n");
     }
 
-    logfile.WriteEx("crtsurfdata2 运行结束。\n");
+    // 模拟生成全国气象站点分钟观测数据，存放在vsurfdata容器中
+    CrtSurfData();
+
+    logfile.WriteEx("crtsurfdata3 运行结束。\n");
 
     return 0;
 }
@@ -113,4 +137,36 @@ bool LoadSite(const char *inifile) {
     }
 
     return true;
+}
+
+// 模拟生成全国气象站点分钟观测数据，存放在vsurfdata容器
+void CrtSurfData() {
+    // 设置随机数种子。
+    srand(time(0));
+
+    // 获取当前时间，当作观测时间
+    char curtime[21];
+    memset(&curtime, 0, sizeof curtime);
+    LocalTime(curtime, "yyyymmddhh24miss");
+
+    struct st_surfdata stsurfdata;
+
+    // 遍历气象站点参数的vstcode容器
+    for (auto& site: vsites) {
+        memset(&stsurfdata,0,sizeof(struct st_surfdata));
+
+        // 用随机数填充分钟观测数据的结构体
+        strncpy(stsurfdata.siteid, site.siteid, 10); // 站号。
+        strncpy(stsurfdata.datatime, curtime, 14);  // 数据时间：格式yyyymmddhh24miss
+        stsurfdata.t = rand() % 351;       // 气温：单位，0.1摄氏度
+        stsurfdata.p = rand() % 265+10000; // 气压：0.1百帕
+        stsurfdata.u = rand() % 100+1;     // 相对湿度，0-100之间的值。
+        stsurfdata.wd = rand() % 360;      // 风向，0-360之间的值。
+        stsurfdata.wf = rand() % 150;      // 风速：单位0.1m/s
+        stsurfdata.r = rand() % 16;        // 降雨量：0.1mm
+        stsurfdata.vis = rand() % 5001 + 100000;  // 能见度：0.1米
+
+        // 把观测数据的结构体放入vsurfdata容器。
+        vsurfdata.push_back(stsurfdata);
+    }
 }
