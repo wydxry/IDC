411390fc4cc2f1ca04c25e8acfd4a49181641de0
wydxry <wydxry@163.com>
2022-06-03 11:18:46 +0800

    演示开发框架操作MySQL数据库（修改表中的记录）

---
+++ public/db/mysql/updatetable.cpp
@@ -0,0 +1,103 @@
+/*
+ *  程序名：updatetable.cpp  此程序演示开发框架操作MySQL数据库（修改表中的记录）
+ *  作者：wydxry
+ *  时间：2022.06.03 10:37
+*/
+
+// 开发框架操作MySQL的头文件
+#include "_mysql.h"       
+
+int main(int argc,char *argv[])
+{
+    // 数据库连接类
+    connection conn;
+
+    // 登录数据库，返回值：0-成功；其它是失败，存放了MySQL的错误代码
+    // 失败代码在conn.m_cda.rc中，失败描述在conn.m_cda.message中。
+    if (conn.connecttodb("127.0.0.1,root,root,mysql,3306", "utf8") != 0) {
+        printf("connect database failed.\n%s\n", conn.m_cda.message); 
+        return -1;
+    }
+
+    // 定义用于超女信息的结构，与表中的字段对应
+    struct st_girls
+    {
+        long id;        // 超女编号
+        char name[31];  // 超女姓名
+        double weight;  // 超女体重
+        char btime[20]; //报名时间
+    }stgirls;
+    
+    // 操作SQL语句的对象
+    sqlstatement stmt(&conn); 
+
+    // 准备修改表的SQL语句
+    // stmt.prepare("\
+    //     update girls set name = :1, weight = :2, btime = str_to_date(:3,'%%Y-%%m-%%d %%H:%%i:%%s') where id= :4");
+    
+    // 另一种修改表的方式
+    stmt.prepare("update girls set name='xxx',weight=27.58,btime=now() where id in (1,3)");
+    stmt.execute();
+    // stmt.m_cda.rpc是本次执行SQL影响的记录数
+    printf("成功修改了%ld条记录。\n", stmt.m_cda.rpc);
+    conn.commit();
+    return 0;
+    
+    /*
+        注意事项：
+        1、参数的序号从1开始，连续、递增，参数也可以用问号表示，但是，问号的兼容性不好，不建议；
+        2、SQL语句中的右值才能作为参数，表名、字段名、关键字、函数名等都不能作为参数；
+        3、参数可以参与运算或用于函数的参数；
+        4、如果SQL语句的主体没有改变，只需要prepare()一次就可以了；
+        5、SQL语句中的每个参数，必须调用bindin()绑定变量的地址；
+        6、如果SQL语句的主体已改变，prepare()后，需重新用bindin()绑定变量；
+        7、prepare()方法有返回值，一般不检查，如果SQL语句有问题，调用execute()方法时能发现；
+        8、bindin()方法的返回值固定为0，不用判断返回值；
+        9、prepare()和bindin()之后，每调用一次execute()，就执行一次SQL语句，SQL语句的数据来自被绑定变量的值。
+    */
+
+    stmt.bindin(1, stgirls.name, 30);
+    stmt.bindin(2, &stgirls.weight);
+    stmt.bindin(3, stgirls.btime, 19);
+    stmt.bindin(4, &stgirls.id);
+
+    // 模拟超女数据，向表中插入5条测试数据
+    for (int i = 0; i < 5; i++) {
+        // 结构体变量初始化
+        memset(&stgirls, 0, sizeof(struct st_girls));
+
+        // 为结构体变量的成员赋值
+        stgirls.id = i;                                     // 超女编号
+        sprintf(stgirls.name, "%50d cute girl", i + 1);     // 超女姓名
+        stgirls.weight = i + 20;                            // 超女体重
+        sprintf(stgirls.btime, "2022-06-03 10:30:%02d", i); // 报名时间
+
+        // 执行SQL语句，一定要判断返回值，0-成功，其它-失败。
+        // 失败代码在stmt.m_cda.rc中，失败描述在stmt.m_cda.message中
+        if (stmt.execute() != 0) {
+            printf("stmt.execute() failed.\n%s\n%s\n", stmt.m_sql, stmt.m_cda.message); 
+            return -1; 
+        } 
+        
+        // stmt.m_cda.rpc是本次执行SQL影响的记录数
+        printf("成功修改了%ld条记录。\n", stmt.m_cda.rpc);
+    }
+
+    printf("update table girls ok.\n");
+
+    // 提交数据库事务
+    conn.commit(); 
+
+    return 0;
+}
+
+/*
+-- 超女基本信息表。
+create table girls(id      bigint(10),    -- 超女编号。
+                   name    varchar(30),   -- 超女姓名。
+                   weight  decimal(8,2),  -- 超女体重。
+                   btime   datetime,      -- 报名时间。
+                   memo    longtext,      -- 备注。
+                   pic     longblob,      -- 照片。
+                   primary key (id));
+*/
