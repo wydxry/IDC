0e3c85a00f2ed75ae414fff2d52b042fcfc1dd0c
wydxry <wydxry@163.com>
2022-06-05 20:21:12 +0800

    启动和终止程序的脚本

---
+++ idc/c/killall.sh
@@ -0,0 +1,10 @@
+####################################################################
+# 停止数据中心后台服务程序的脚本
+####################################################################
+
+killall -9 procctl
+killall gzipfiles crtsurfdata deletefiles 
+
+sleep 3
+
+killall -9 gzipfiles crtsurfdata deletefiles 
