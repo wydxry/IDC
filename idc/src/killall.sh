93958d513efe8f5b0ac931a96ae4bcbf88ce16dd
wydxry <wydxry@163.com>
2022-09-26 11:07:49 +0800

    update

--- idc/src/killall.sh
+++ idc/src/killall.sh
@@ -4,7 +4,13 @@
 
 killall -9 procctl
 killall gzipfiles crtsurfdata deletefiles ftpgetfiles ftpputfiles tcpputfiles tcpgetfiles fileserver
+killall obtcodetodb obtmindtodb execsql dminingmysql xmltodb syncupdate syncincrement
+killall deletetable migratetable xmltodb_oracle migratetable_oracle deletetable_oracle
+killall syncupdate_oracle syncincrement_oracle 
 
 sleep 3
 
 killall -9 gzipfiles crtsurfdata deletefiles ftpgetfiles ftpputfiles tcpputfiles tcpgetfiles fileserver
+killall -9 obtcodetodb obtmindtodb execsql dminingmysql xmltodb syncupdate syncincrement
+killall -9 deletetable migratetable xmltodb_oracle migratetable_oracle deletetable_oracle
+killall -9 syncupdate_oracle syncincrement_oracle
